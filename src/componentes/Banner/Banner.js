ffe13142409656f49fe329d41465a455a016bb63
Guilherme Barros JR <131307334+dida0982@users.noreply.github.com>
2024-02-08 14:33:37 -0300

    primeiro commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,15 @@
+import "./Banner.css";
+
+export const Banner = () => {
+  //JSX
+  return (
+    <headers className="banner">
+      <img
+        src="/imagens/BannerRodrigo.png"
+        alt="O banner principal da página do Organo"
+      />
+    </headers>
+  );
+};
+
+export default Banner;
